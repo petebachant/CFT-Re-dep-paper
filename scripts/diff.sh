2b18ba3e74d6df882bd6be919f5b97db1ea9bf8a
Pete Bachant <petebachant@gmail.com>
2015-12-16 12:01:57 -0500

    Diff script working

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+# This script creates a diff version comparing the recent with whatever revision
+# is supplied as the first argument. This can be any valid Git identifier, e.g.,
+# a commit hash, tag, etc.
 
 rev=$1
 git show $rev:paper.tex > Archive/paper-$rev.tex
-latexdiff Archive/paper-$rev.tex paper.tex > paper-$rev-diff.tex
-# latexmk paper-$rev-diff.tex -pdf -f
+latexdiff --exclude-textcmd="section,subsection,subsubsection" --math-markup=off Archive/paper-$rev.tex Archive/paper-unix.tex > paper-$rev-diff.tex
+latexmk paper-$rev-diff.tex -pdf -f
+mv paper-$rev-diff.pdf Archive
+rm paper-$rev*
